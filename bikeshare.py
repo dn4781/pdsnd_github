fe407a7b884143a78b58b541e244887977675df6
DN4781 <DN4781@att.com>
2020-04-22 22:43:03 -0500

    Updated Input comments

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
 #def get_filters():
 
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # Get user input for city (chicago, new york city, washington)
 #while True:
     while True:
         city = input("What city would you like to research: Chicago, New York or Washington? \n").lower()
